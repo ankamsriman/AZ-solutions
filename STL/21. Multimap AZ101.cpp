1481ce2a508fe34e149034e43486c29ac5e2f286
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-06-13 03:28:58 +0530

    Create 21. Multimap AZ101.cpp

---
+++ STL/21. Multimap AZ101.cpp
@@ -0,0 +1,99 @@
+//ANKAM SRIMAN
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define fi(i, a, b) for (long long int i = a; i < b; i++)
+#define fd(i, a, b) for (long long int i = a; i >= b; i--)
+#define vl(a, n) vector<ll> a(n)
+#define scarr(a) for (long long int i = 0; i < a.size(); i++) cin >> a[i]
+#define parr(a)  for (long long int i = 0; i < a.size(); i++) cout << a[i] << " "
+#define prr(a)  for (auto x:a) cout << x << " "
+#define vt(a,n) vector<long long int>a(n)
+#define ll long long
+#define ull unsigned long long int
+// transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+#define spa " "
+#define nl cout << "\n"
+#define pr(x) cout << x << "\n"
+#define star cout << "*"
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
+#define f first
+#define s second
+#define ii pair<ll, ll>
+
+const ll mod = 1e9 + 7;
+const ll N = 100000;
+
+
+void solve()
+{
+    ll q;
+    cin>>q;
+    multimap<string,ll>mp;
+    while(q--)
+    {
+        string s;
+        cin>>s;
+        if(s=="add")
+        {
+            string x;
+            ll y;
+            cin>>x>>y;
+            mp.insert({x,y});
+        }
+        else if(s=="erase")
+        {
+            string x;
+            cin>>x;
+            if(mp.find(x)!=mp.end())mp.erase(mp.find(x));
+        }
+        else if(s=="eraseall")
+        {
+            string x;
+            cin>>x;
+            if(mp.find(x)!=mp.end())mp.erase(x);   
+        }
+        else
+        {
+            string x;
+            cin>>x;
+            if(mp.find(x)!=mp.end())
+            {
+                auto it=mp.find(x);
+                cout<<(it->second);
+                nl;   
+            }
+            else cout<<"0\n";
+            
+
+        }
+    }
+}   
+
+
+signed main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    IOS;
+
+    ll t = 1;
+    cin >> t;
+    // string x;
+    // getline(cin,x);
+    while (t--)
+    {
+        // q++;
+
+        solve();
+    }
+    return 0;
+}
