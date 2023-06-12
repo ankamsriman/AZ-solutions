d7a37043ed65f154382d15245e989682df3daefc
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-06-12 23:28:05 +0530

    Update 11. Deque AZ101.cpp

--- STL/11. Deque AZ101.cpp
+++ STL/11. Deque AZ101.cpp
@@ -1,21 +1,13 @@
 //ANKAM SRIMAN
 #include <bits/stdc++.h>
 using namespace std;
-
-#define IOS                      
-    ios::sync_with_stdio(false); 
-    cin.tie(NULL);               
-    cout.tie(NULL);
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 #define fi(i, a, b) for (long long int i = a; i < b; i++)
 #define fd(i, a, b) for (long long int i = a; i >= b; i--)
 #define vl(a, n) vector<ll> a(n)
-#define scarr(a)                                 
-    for (long long int i = 0; i < a.size(); i++) 
-    cin >> a[i]
-#define parr(a)                                  
-    for (long long int i = 0; i < a.size(); i++) 
-    cout << a[i] << " "
+#define scarr(a) for (long long int i = 0; i < a.size(); i++) cin >> a[i]
+#define parr(a)  for (long long int i = 0; i < a.size(); i++) cout << a[i] << " "
 #define ll long long
 #define ull unsigned long long int
 // transform(s.begin(), s.end(), s.begin(), ::tolower);
