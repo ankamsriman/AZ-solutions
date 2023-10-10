82d5d7118cd8e58b1f984807920923e03675377c
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-10-10 17:37:23 +0530

    Create 9. Queue AZ101.cpp

---
+++ Appliend c++/9. Queue AZ101.cpp
@@ -0,0 +1,157 @@
+//ANKAM SRIMAN
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define fi(i,a,b) for(long long int i = a; i<b; i++)
+#define fd(i,a,b) for(long long int i = a; i>=b; i--)
+#define scarr(a) for(long long int i=0;i<a.size();i++) cin>>a[i]
+#define parr(a) for(long long int i=0;i<a.size();i++) cout<<a[i]<<" "
+#define ll  long long int
+#define ull unsigned long long int
+//transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+#define spa " "
+#define nl cout<<"\n"
+#define pr(x) cout<<x<<"\n"
+#define prvt(a) for(long long int i=0;i<a.size();i++) cout<<a[i]<<" "
+#define star cout<<"*\n"
+
+
+//string s;
+//ll a[1000009];
+
+
+//vector<ll>a,b;
+//ll n, k;
+
+//ll mod = 1000000007;
+ll is_prime[1000100];
+ll sp[1000100]; 
+ll N = 1000100;
+
+ll q = 0;
+
+const ll mod = 1e9 + 7;
+
+void solve()
+
+{
+
+    //star;
+    queue<ll>qt;
+    ll q;
+    cin>>q;
+    while(q--)
+    {
+        string s;
+        cin>>s;
+        if(s=="print")
+        {
+            if(! qt.empty())
+            {
+                cout<<qt.front();
+            }
+            else
+            {
+                cout<<"0";
+            }
+            nl;
+        }
+        else if(s=="add")
+        {
+            ll x;
+            cin>>x;
+            qt.push(x);
+        }
+        else if(s=="remove")
+        {
+            if(!qt.empty()) qt.pop();
+        }
+    }
+
+
+}
+//cout<<fixed<<setprecision(7);
+int main()
+{
+    //py;
+    //fun 
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    //fac();
+
+    IOS;
+    //cout<<"*";
+    ll t=1;
+    cin >> t; 
+    //getline(cin,s);
+    while (t--)
+    {
+
+        //q++;
+        
+        solve();
+    }
+    return 0 ;
+}
+// Description
+
+// You are given Q queries and have to perform the following operations:
+
+// add x - add element x at the end of queue
+// remove- delete front element of the queue, if queue is not empty
+// print - print the element at the first position, if queue is not empty, otherwise print 0
+
+// Input Format
+
+// The first line of the input contains one integer T - the number of test cases. Then T test cases follow.
+
+// The first line of each test case contains one integer Q - the number of queries.
+
+// Each of the next Q ines contains queries.
+
+
+// Output Format
+
+// For each test case, print the required queries.
+
+
+// Constraints
+
+// 1≤ T ≤ 106
+
+// 1≤ Q ≤ 106
+
+// 1≤ X ≤ 106
+
+// It is guaranteed that the sum of Q over all test cases does not exceed 106
+
+
+// Sample Input 1
+
+// 1
+// 5
+// add 3
+// add 4
+// print
+// remove
+// print
+
+// Sample Output 1
+
+// 3
+// 4
+
+// Note
+
+// Queue becomes [3]
+// Queue becomes [3, 4]
+// Element at front = 3
+// Queue becomes [4]
+// Element at front = 4
