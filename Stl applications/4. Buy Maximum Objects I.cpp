7c201b050ff91ac6c76cc0368b69dd0e101981fe
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-10-12 18:17:05 +0530

    Update 4. Buy Maximum Objects I.cpp

--- Stl applications/4. Buy Maximum Objects I.cpp
+++ Stl applications/4. Buy Maximum Objects I.cpp
@@ -72,3 +72,67 @@ signed main()
     }
     return 0;
 }
+// Description
+
+// There are N objects, and each object has some price. The price of ith object is Ai.
+// You have total money M, and you want to buy as many objects as possible.
+// Find the maximum objects that you can buy?
+
+
+// Input Format
+
+// The first line contains T, the number of test cases.
+// The first line of each test case contains N, the number of objects, and M, the money you have.
+// The second line of each test case contains N space-separated integers A1, A2, ..., AN.
+
+
+// Output Format
+
+// For each test case, print a single number representing the maximum objects that you can buy.
+
+
+// Constraints
+
+// 1 ≤ T ≤ 105
+// 1 ≤ N ≤ 105
+// 1 ≤ Ai ≤ 108
+// 1 ≤ M ≤ 108
+// Sum of N over all test cases ≤ 5 * 105.
+
+
+// Sample Input 1
+
+// 3
+
+// 5 5
+
+// 1 3 2 4 5
+
+// 5 10
+
+// 1 3 2 4 5
+
+// 5 20
+
+// 1 3 2 4 5
+
+
+// Sample Output 1
+
+// 2
+
+// 4
+
+// 5
+
+
+// Note
+
+// Explanation 1:
+// Buy Objects with prices 1, 2.
+
+// Explanation 2:
+// Buy Objects with prices 1, 2, 3, 4.
+
+// Explanation 3:
+// Buy all objects.
