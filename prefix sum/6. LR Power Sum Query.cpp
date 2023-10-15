29052ec6644ca94cb565dbff2c9af05b8dc27d19
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-10-15 20:06:48 +0530

    Update 6. LR Power Sum Query.cpp

--- prefix sum/6. LR Power Sum Query.cpp
+++ prefix sum/6. LR Power Sum Query.cpp
@@ -101,3 +101,49 @@ signed main()
     }
     return 0;
 }
+// Description
+
+// Given an array of N integers, Q queries, and an integer K. In each query two integers L, R is given, you have to find (A[L] + A[L+1] * K + A[L+2] * K^2 + …. A[R] * K^(R-L))% 10^9+7.
+
+
+// Input Format
+
+// The first line contains three space-separated integers N, Q, K where 1<=N<=10^6, 1<=Q<=10^6, 1<=K<=10^9.
+
+// Next line contains N space-separated integers (-1e9<=Ai<=1e9).
+
+// Next Q lines contain two space-separated integers L, R where 1<=L<=R<=N.
+
+
+// Output Format
+
+// For each query print the value of (A[L] + A[L+1] * K + A[L+2] * K^2 + …. A[R] * K^(R-L))% 10^9+7 in a new line.
+
+
+// Sample Input 1
+
+// 10 10 234565
+// 12 -23 -123 2345 2345 44 345 -93945 -5353 1
+// 1 5
+// 1 8
+// 1 10
+// 2 2
+// 6 6
+// 4 8
+// 2 9
+// 4 10
+// 3 6
+// 5 9
+
+// Sample Output 1
+
+// 644011223
+// 645979052
+// 534876628
+// 999999984
+// 44
+// 725883751
+// 503173837
+// 859246404
+// 845338365
+// 605774687
