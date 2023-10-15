51dd98dfc0b9e6646fb5d251511b25996144eb9b
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-10-15 20:06:15 +0530

    Update 5. Multiply Sum Query.cpp

--- prefix sum/5. Multiply Sum Query.cpp
+++ prefix sum/5. Multiply Sum Query.cpp
@@ -78,3 +78,50 @@ signed main()
     }
     return 0;
 }
+
+// Description
+
+// Given an array of N integers and Q queries. In each query two integers L, R is given, you have to find (A[L] + A[L+1]*2 + A[L+2]*3 + A[L+3]*4...A[R]*(R-L+1)) % 10^9+7.
+
+
+// Input Format
+
+// The first line contains two space-separated integers N, Q where 1<=N<=10^6, 1<=Q<=10^6.
+
+// Next line contains N space-separated integers (-1e9<=Ai<=1e9).
+
+// Next Q lines contain two space-separated integers L, R where 1<=L<=R<=N.
+
+
+// Output Format
+
+// For each query print the value of (A[L] + A[L+1]*2 + A[L+2]*3 + A[L+3]*4...A[R]*(R-L+1)) % 10^9+7 in a new line.
+
+
+// Sample Input 1
+
+// 10 10
+// 12 -23 -123 2345 2345 44 345 -93945 -5353 1
+// 1 5
+// 1 8
+// 1 10
+// 2 2
+// 6 6
+// 4 8
+// 2 9
+// 4 10
+// 3 6
+// 5 9
+
+// Sample Output 1
+
+// 20702
+// 999271828
+// 999223661
+// 999999984
+// 44
+// 999538829
+// 999318004
+// 999506718
+// 11778
+// 999600930
