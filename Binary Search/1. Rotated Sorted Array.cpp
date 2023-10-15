de07ff49f2ffe6c7f607a4b4d2107b6c8a8f8ef5
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-10-16 03:20:06 +0530

    Update 1. Rotated Sorted Array.cpp

--- Binary Search/1. Rotated Sorted Array.cpp
+++ Binary Search/1. Rotated Sorted Array.cpp
@@ -77,3 +77,52 @@ signed main()
     }
     return 0;
 }
+
+// Description
+
+// Given a rotated sorted array. Find the index of the minimum element in the array.
+
+// All the elements are distinct.
+
+ 
+
+
+// Input Format
+
+// The first line contains T, the number of test cases (1<=T<=10000).
+
+// The first line contains an integer N where 1<=N<=10^5.
+
+// The second line contains N space-separated integers A1,A2....AN where -1e9<=Ai<=1e9.
+
+// Sum of N over all test cases ≤10^6.
+
+
+// Output Format
+
+// For each test case print the index of the minimum element in the array.
+
+// 0-indexed.
+
+
+// Sample Input 1
+
+// 5
+// 5
+// 3 4 5 1 2
+// 5
+// 4 5 1 2 3
+// 5
+// 5 1 2 3 4
+// 5
+// 1 2 3 4 5
+// 5
+// 2 3 4 5 1
+
+// Sample Output 1
+
+// 3
+// 2
+// 1
+// 0
+// 4
