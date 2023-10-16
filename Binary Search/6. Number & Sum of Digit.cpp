940ddf3a52f7fe8905903aada3f401ec036b8185
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-10-16 20:32:53 +0530

    Update 6. Number & Sum of Digit.cpp

--- Binary Search/6. Number & Sum of Digit.cpp
+++ Binary Search/6. Number & Sum of Digit.cpp
@@ -94,3 +94,47 @@ signed main()
     }
     return 0;
 }
+
+// Description
+
+// You are given two integers N and S. Find the number of positive integers X less than equal to N, such that the difference between X and the sum of its digits (in decimal representation) is not less than S.
+
+
+// Input Format
+
+// The first line of input contains one integer T (1 ≤ T ≤ 104) — the number of test cases. Then T test cases follow.
+
+// The first line of each test case contains two space-separated integer N, S where 1<=N<=10^18, 0<=S<=10^18.
+
+
+// Output Format
+
+// For each test case print the number of positive integers X in a new line.
+
+
+// Sample Input 1
+
+// 10
+// 5 4
+// 100 5
+// 20 3
+// 40 19
+// 32 15
+// 69 12
+// 1000000000000000 0
+// 1000000000000000000 10000000
+// 9999999999999 9999999999882
+// 13465751690089037 981448507916936
+
+// Sample Output 1
+
+// 0
+// 91
+// 11
+// 11
+// 13
+// 50
+// 1000000000000000
+// 999999999989999991
+// 10
+// 12484303182172038
