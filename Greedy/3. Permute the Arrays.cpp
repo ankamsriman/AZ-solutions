f370faade60ed71966d1c438dd37c8addce4d8d6
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-10-30 23:36:42 +0530

    Create 3. Permute the Arrays.cpp

---
+++ Greedy/3. Permute the Arrays.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define lli long long int
+
+void solve(){
+	int n;
+	cin >> n;
+	int arr[n];
+	int brr[n];
+	for(int i=0;i<n;i++)cin >> arr[i];
+	for(int i=0;i<n;i++)cin >> brr[i];
+	sort(arr,arr+n);
+	sort(brr,brr+n);
+	lli ans=0;
+	for(int i=0;i<n;i++){
+		ans+=abs(arr[i]-brr[i]);
+	}
+
+	cout<< ans << endl;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin >>t;
+	while(t--){
+		solve();
+	}
+
+}
+// Time-Limit: 1 sec Score: 100/100
+// Difficulty : 
+//  Memory: 256 MB Accepted Submissions: 100
+// Description
+
+// There are two arrays of size n, A [a1, a2 … an] and B [b1, b2 … bn]. You can permute the elements in both the arrays any way you want.
+
+// Your goal is to minimize the sum of the absolute differences between corresponding elements of the arrays i.e. sum of |ai-bi| for i from 1 to n.
+
+// Find this minimum sum.
+
+
+// Input Format
+
+// First line of input contains T, number of test cases.
+
+// First line of each test case contains n, size of the arrays.
+
+// Second line contains n integers a1, a2 … an .
+
+// Third line contians n integers b1, b2 … bn .
+
+ 
+
+ 
+
+
+// Output Format
+
+// For each test case print a single integer in a new line: Minimum sum of the absolute differences between corresponding elements of the arrays.
+
+
+// Constraints
+
+// T ≤ 10
+
+// 1 ≤ n ≤ 105
+
+// 0 ≤ |ai|,|bi| ≤ 106
+
+
+// Sample Input 1
+
+// 1
+
+// 3
+
+// 1 2 1
+
+// 2 1 1
+
+
+// Sample Output 1
+
+// 0
+
+
+// Note
+
+// Arrange elements in B as [1, 2, 1].
+
+// Sum = |1-1| + |2-2| + |1-1| = 0
