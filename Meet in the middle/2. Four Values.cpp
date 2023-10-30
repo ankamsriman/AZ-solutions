98662f2a3f2d2c2f87aa0b685379668b031b357c
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-10-30 17:26:36 +0530

    Create 2. Four Values.cpp

---
+++ Meet in the middle/2. Four Values.cpp
@@ -0,0 +1,128 @@
+//ANKAM SRIMAN
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define fi(i, a, b) for (long long int i = a; i < b; i++)
+#define fd(i, a, b) for (long long int i = a; i >= b; i--)
+#define vl(a, n) vector<ll> a(n)
+#define scarr(a) for (long long int i = 0; i < a.size(); i++) cin >> a[i]
+#define parr(a)  for (long long int i = 0; i < a.size(); i++) cout << a[i] << " "
+#define prr(a)  for (auto x:a) cout << x << " "
+#define vt(a,n) vector<long long int>a(n)
+#define ll long long
+#define ull unsigned long long int
+#define pb(x) push_back(x)
+// transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+#define spa " "
+#define nl cout << "\n"
+#define pr(x) cout << x << "\n"
+#define star cout << "*"
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
+#define f first
+#define s second
+#define ii pair<ll,ll>
+
+
+using state =pair<int,int>;
+const ll mod = 1e9 + 7;
+const ll N = 100000;
+
+vector<ll>v;
+ll n,t;
+
+bool fun()
+{
+    unordered_map<ll,ll>mp;
+    fd(b,n-3,1)
+    {
+        ll c=b+1;
+        fi(d,c+1,n)
+        {
+            mp[v[d]+v[c]]=1;
+        }
+        fd(a,b-1,0)
+        {
+            if(mp[t-v[a]-v[b]])
+            {
+                // cout<<v[a]<<" "<<v[b]<<" "<<t-v[a]-v[b];
+                return 1;;
+            }
+        }
+    }
+    return 0;
+}
+
+
+void solve()
+{
+    // ll n,t;
+    cin>>n>>t;
+    // vt(v,n);
+    v.resize(n);
+    scarr(v);
+    if(fun())cy;
+    else cn;
+
+
+}
+
+signed main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    IOS;
+    ll t = 1;
+    // cin >> t;
+    // string x;
+    // getline(cin,x);
+    while (t--)
+    {
+        // q++;
+
+        solve();
+    }
+    return 0;
+}
+
+// Description
+
+// You are given an array of n integers, and your task is to find four values (at distinct positions) whose sum is x.
+
+
+// Input Format
+
+// The first input line has two integers n and x: the array size and the target sum.
+// The second line has n integers a1, a2, …,an: the array values.
+
+
+// Output Format
+
+// Print 'YES' if such four values exist, otherwise 'NO'.
+
+
+// Constraints
+
+// 1 ≤ n ≤ 1000
+// 1 ≤ x, ai ≤ 109
+
+
+// Sample Input 1
+
+// 8 15
+// 3 2 5 8 1 3 2 3
+
+// Sample Output 1
+
+// YES
+
+// Note
+
+// Four values are {2, 8, 3, 2}.
